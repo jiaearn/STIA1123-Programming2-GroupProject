21ec4385764241c78989999907eb23960c150fca
Tan Jia Earn <60874230+jiaearn@users.noreply.github.com>
2021-10-13 00:31:56 +0800

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,10 @@
+typeSearchIndex = [{"p": "my.javatree", "l": "AddData"}, {
+    "l": "All Classes",
+    "url": "allclasses-index.html"
+}, {"p": "my.example", "l": "App"}, {"p": "my.javatree", "l": "Calculation"}, {
+    "p": "my.javatree",
+    "l": "CoronaVirus"
+}, {"p": "my.javatree", "l": "DataOfSystem"}, {"p": "my.javatree", "l": "DisplayData"}, {
+    "p": "my.javatree",
+    "l": "EditData"
+}, {"p": "my.javatree", "l": "MainMenu"}]
